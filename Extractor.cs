8f18ad371c6d0185385d76ff83eabb0d087ba825
Ondrej Masita <masiton@users.noreply.github.com>
2024-07-26 17:15:26 +0200

    Add files via upload

---
+++ Extractor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System.Collections.Generic;
+
+namespace OutlookContactsExtractor
+{
+    public class Extractor
+    {
+        public List<Contact> GetContacts()
+        {
+            var contacts = new List<Contact>();
+
+            // https://stackoverflow.com/questions/17010963/how-to-get-the-contacts-of-outlook-to-c
+            Microsoft.Office.Interop.Outlook.Items OutlookItems;
+            Microsoft.Office.Interop.Outlook.Application outlookObj;
+            MAPIFolder Folder_Contacts;
+
+            outlookObj = new Microsoft.Office.Interop.Outlook.Application();
+            Folder_Contacts = (MAPIFolder)outlookObj.Session.GetDefaultFolder(OlDefaultFolders.olFolderContacts);
+            OutlookItems = Folder_Contacts.Items;
+
+            for (int i = 0; i < OutlookItems.Count; i++)
+            {
+                Microsoft.Office.Interop.Outlook.ContactItem contact = (Microsoft.Office.Interop.Outlook.ContactItem)OutlookItems[i + 1];
+                var c = new Contact()
+                {
+                    Name = contact.FirstName,
+                    Surname = contact.LastName,
+                    Phones = GetPhones(contact),
+                    Emails = GetEmails(contact),
+                    Company = contact.CompanyName,
+                };
+
+                contacts.Add(c);
+            }
+
+            return contacts;
+        }
+
+        private string[] GetPhones(ContactItem contact)
+        {
+            var phones = new List<string>();
+
+            AddIfNotNull(contact.BusinessTelephoneNumber);
+            AddIfNotNull(contact.HomeTelephoneNumber);
+            AddIfNotNull(contact.MobileTelephoneNumber);
+            AddIfNotNull(contact.OtherTelephoneNumber);
+
+            return phones.ToArray();
+
+            void AddIfNotNull(string phone)
+            {
+                if (string.IsNullOrWhiteSpace(phone) == false)
+                {
+                    phones.Add(phone);
+                }
+            }
+        }
+
+        private string[] GetEmails(ContactItem contact)
+        {
+            var phones = new List<string>();
+
+            AddIfNotNull(contact.Email1Address);
+            AddIfNotNull(contact.Email2Address);
+            AddIfNotNull(contact.Email3Address);
+
+            return phones.ToArray();
+
+            void AddIfNotNull(string phone)
+            {
+                if (string.IsNullOrWhiteSpace(phone) == false)
+                {
+                    phones.Add(phone);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using FolkerKinzel.VCards;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OutlookContactsExtractor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var extractor = new Extractor();
+            var contacts = extractor.GetContacts();
+
+            var cards = new List<VCard>();
+
+            foreach (var contact in contacts ) 
+            {
+                var card = new VCard();
+                var builder = VCardBuilder.Create(card);
+
+                contact.Phones.ToList().ForEach(x => builder.Phones.Add(x));
+                contact.Emails.ToList().ForEach(x => builder.EMails.Add(x));
+                builder.NameViews.Add(new[] { contact.Surname }, new[] {contact.Name});
+
+                if(string.IsNullOrWhiteSpace(contact.Company) == false)
+                {
+                    builder.Organizations.Add(new FolkerKinzel.VCards.Models.Organization(contact.Company));
+                }
+
+                cards.Add(card);
+            }
+
+            var filename = $"{Guid.NewGuid()}.vcf";
+            using (var stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+            {
+                Vcf.Serialize(cards, stream);
+            }
+
+        }
+    }
+}
